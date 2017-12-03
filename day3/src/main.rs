b55de9eb7cd6f9b9d8c4c575b39dc4c793849e78
Graham Lowe <graham.lowe@gmail.com>
2017-12-03 01:15:39 -0500

    Add solution for day 3.

---
+++ day3/src/main.rs
@@ -0,0 +1,162 @@
+use std::collections::HashMap;
+
+#[derive(Clone, Debug)]
+enum CardinalDirection {
+    North,
+    East,
+    South,
+    West,
+}
+use CardinalDirection::*;
+
+fn num_steps(square: u32) -> i32 {
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut steps = 0;
+    let mut incr = 1;
+    let mut direction = East;
+    let mut times_matched = 0;
+    let mut squares = HashMap::new();
+    squares.insert(1, (0, 0));
+    for s in 2..(square + 1) {
+        match direction {
+            East => x += 1,
+            North => y += 1,
+            West => x -= 1,
+            South => y -= 1,
+        }
+        squares.insert(s, (x, y));
+
+        steps += 1;
+        if steps == incr {
+            // turn
+            steps = 0;
+            direction = match direction {
+                East => North,
+                North => West,
+                West => South,
+                South => East,
+            };
+            times_matched += 1;
+            if times_matched % 2 == 0 {
+                incr += 1;
+            }
+
+        }
+    }
+    x.abs() + y.abs()
+}
+
+fn first_value_larger(input: u32) -> u32 {
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut steps = 0;
+    let mut incr = 1;
+    let mut direction = East;
+    let mut times_matched = 0;
+    let mut squares = HashMap::new();
+    let mut values = HashMap::new();
+    squares.insert(1, (0, 0));
+    values.insert((0, 0), 1);
+    let mut value = 0;
+    for s in 2..100000 {
+        match direction {
+            East => x += 1,
+            North => y += 1,
+            West => x -= 1,
+            South => y -= 1,
+        }
+        squares.insert(s, (x, y));
+        // sum neighbor values
+        value = 0;
+        // east
+        if let Some(v) = values.get(&(x + 1, y)) {
+            value += v;
+        }
+        // north east
+        if let Some(v) = values.get(&(x + 1, y + 1)) {
+            value += v;
+        }
+        // north
+        if let Some(v) = values.get(&(x, y + 1)) {
+            value += v;
+        }
+        // north west
+        if let Some(v) = values.get(&(x - 1, y + 1)) {
+            value += v;
+        }
+        // west
+        if let Some(v) = values.get(&(x - 1, y)) {
+            value += v;
+        }
+        // south west
+        if let Some(v) = values.get(&(x - 1, y - 1)) {
+            value += v;
+        }
+        // south
+        if let Some(v) = values.get(&(x, y - 1)) {
+            value += v;
+        }
+        // south east
+        if let Some(v) = values.get(&(x + 1, y - 1)) {
+            value += v;
+        }
+
+
+        values.insert((x, y), value);
+//        println!("({},{}) = {}", x, y, value);
+
+        if value > input {
+            return value;
+        }
+
+        steps += 1;
+        if steps == incr {
+            // turn
+            steps = 0;
+            direction = match direction {
+                East => North,
+                North => West,
+                West => South,
+                South => East,
+            };
+            times_matched += 1;
+            if times_matched % 2 == 0 {
+                incr += 1;
+            }
+
+        }
+    }
+    value
+}
+
+fn main() {
+    println!("part 1: {}", num_steps(368078));
+    println!("part 1: {}", first_value_larger(368078));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_num_steps() {
+        assert_eq!(0, num_steps(1));
+        assert_eq!(3, num_steps(12));
+        assert_eq!(2, num_steps(23));
+        assert_eq!(31, num_steps(1024));
+    }
+
+    #[test]
+    fn test_first_value_larger() {
+        assert_eq!(2, first_value_larger(1));
+        assert_eq!(4, first_value_larger(2));
+        assert_eq!(10, first_value_larger(5));
+        assert_eq!(11, first_value_larger(10));
+        assert_eq!(23, first_value_larger(11));
+        assert_eq!(25, first_value_larger(23));
+        assert_eq!(26, first_value_larger(25));
+        assert_eq!(54, first_value_larger(26));
+    }
+
+}
