4bea91cac900014d4f20b137b41bc01ed3b0f69d
Graham Lowe <graham.lowe@gmail.com>
2017-12-06 15:44:26 -0500

    Use sum in day1.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,32 +1,36 @@
 use std::io;
 
-fn sum_match_offset(captcha: &str, offset: usize) -> u32 {
-    let digits: Vec<_> = captcha.chars().map(|c| c.to_digit(10).unwrap()).collect();
-    let len = digits.len();
-    (0..len).fold(0, |sum, i| {
-        if digits[i] == digits[(i + offset) % len] {
-            sum + digits[i]
-        } else {
-            sum
-        }
-    })
-}
-
-fn sum_match_next(captcha: &str) -> u32 {
-    sum_match_offset(&captcha, 1)
-}
-
-fn sum_match_halfway_around(captcha: &str) -> u32 {
-    sum_match_offset(&captcha, captcha.len() / 2)
-}
-
 fn main() {
-    let mut captcha = String::new();
-    io::stdin().read_line(&mut captcha).unwrap();
-    // pop the newline.
-    captcha.pop();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    // Strip the newline.
+    input.pop();
+    let captcha: Vec<u32> = parse(&input);
     println!("part one: {}", sum_match_next(&captcha));
     println!("part two: {}", sum_match_halfway_around(&captcha));
+}
+
+fn parse(input: &str) -> Vec<u32> {
+    input.chars().map(|c| c.to_digit(10).unwrap()).collect()
+}
+
+fn sum_match_next(captcha: &Vec<u32>) -> u32 {
+    sum_match_offset(captcha, 1)
+}
+
+fn sum_match_halfway_around(captcha: &Vec<u32>) -> u32 {
+    sum_match_offset(captcha, captcha.len() / 2)
+}
+
+fn sum_match_offset(captcha: &Vec<u32>, offset: usize) -> u32 {
+    let n = captcha.len();
+    (0..n)
+        .map(|i| if captcha[i] == captcha[(i + offset) % n] {
+            captcha[i]
+        } else {
+            0
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -34,19 +38,24 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse() {
+        assert_eq!(parse("1111"), vec![1, 1, 1, 1])
+    }
+
+    #[test]
     fn test_sum_match_next() {
-        assert_eq!(3, sum_match_next("1122"));
-        assert_eq!(4, sum_match_next("1111"));
-        assert_eq!(0, sum_match_next("1234"));
-        assert_eq!(9, sum_match_next("91212129"));
+        assert_eq!(3, sum_match_next(&vec![1, 1, 2, 2]));
+        assert_eq!(4, sum_match_next(&vec![1, 1, 1, 1]));
+        assert_eq!(0, sum_match_next(&vec![1, 2, 3, 4]));
+        assert_eq!(9, sum_match_next(&vec![9, 1, 2, 1, 2, 1, 2, 9]));
     }
 
     #[test]
     fn test_sum_match_halfway_around() {
-        assert_eq!(6, sum_match_halfway_around("1212"));
-        assert_eq!(0, sum_match_halfway_around("1221"));
-        assert_eq!(4, sum_match_halfway_around("123425"));
-        assert_eq!(12, sum_match_halfway_around("123123"));
-        assert_eq!(4, sum_match_halfway_around("12131415"));
+        assert_eq!(6, sum_match_halfway_around(&vec![1, 2, 1, 2]));
+        assert_eq!(0, sum_match_halfway_around(&vec![1, 2, 2, 1]));
+        assert_eq!(4, sum_match_halfway_around(&vec![1, 2, 3, 4, 2, 5]));
+        assert_eq!(12, sum_match_halfway_around(&vec![1, 2, 3, 1, 2, 3]));
+        assert_eq!(4, sum_match_halfway_around(&vec![1, 2, 1, 3, 1, 4, 1, 5]));
     }
 }
