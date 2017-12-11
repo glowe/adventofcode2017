0816533f948518166c39db56d8d8177e6c235882
Graham Lowe <graham.lowe@gmail.com>
2017-12-10 19:45:34 -0500

    Add full solution for day 7.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,20 +1,104 @@
-#[macro_use]
-extern crate lazy_static;
+/// This code is heavily inspired from Peter Norvig's solution.  I was
+/// having a lot of trouble with this problem, because I thought we
+/// were supposed to find the correct weight for a program in the
+/// second level of the tower. What we are supposed to do is try to
+/// "balance" as hight up in the tower as possible.
+
 extern crate regex;
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::process;
 use std::fs::File;
 use std::io::{BufReader, Read, Result};
 use regex::Regex;
 
-
 #[derive(Debug)]
-struct Program {
-    name: String,
-    weight: u32,
-    children: Vec<String>,
+struct Tower<'a> {
+    weight: HashMap<&'a str, u32>,
+    above: HashMap<&'a str, Vec<&'a str>>,
+}
+
+impl<'a> Tower<'a> {
+    fn base(&self) -> &str {
+        // The base is the program that doesn't appear above anything
+        let above: HashSet<&str> = self.above
+            .values()
+            .flat_map(|&ref a| a.clone())
+            .collect();
+        self.above
+            .keys()
+            .find(|&key| !above.contains(key))
+            .unwrap()
+    }
+
+    // TODO: error handling if given bad name
+    fn tower_weight(&self, name: &str) -> u32 {
+        let weight = self.weight.get(name).unwrap();
+        let above_weight: u32 = self.above
+            .get(name)
+            .unwrap()
+            .iter()
+            .map(|above| self.tower_weight(above))
+            .sum();
+        weight + above_weight
+    }
+
+    // TODO: error handling if given bad name
+    fn siblings(&self, name: &str) -> HashSet<&str> {
+        match self.above
+            .values()
+            .find(|above| above.contains(&name)) {
+            None => HashSet::new(),
+            Some(above) => {
+                above.iter()
+                    .filter(|&n| *n != name)
+                    .map(|n| *n)
+                    .collect()
+            }
+        }
+    }
+
+    fn wrong(&self, name: &str) -> bool {
+        if name == self.base() {
+            false
+        } else {
+            // weight is not the weight of its siblings
+            self.siblings(name)
+                .iter()
+                .map(|sibling| self.tower_weight(sibling))
+                .find(|&weight| self.tower_weight(name) == weight)
+                .is_none()
+        }
+    }
+
+    fn wrongest(&self) -> &str {
+        let programs: Vec<&str> = self.programs();
+        programs.into_iter()
+            .find(|name| {
+                self.wrong(name) &&
+                !self.above
+                    .get(name)
+                    .unwrap()
+                    .iter()
+                    .any(|above| self.wrong(above))
+            })
+            .unwrap()
+    }
+
+    fn correct_weight(&self, name: &str) -> u32 {
+        let sibling = self.siblings(name)
+            .into_iter()
+            .take(1)
+            .collect::<Vec<&str>>()[0];
+        let delta = self.tower_weight(sibling) as i32 - self.tower_weight(name) as i32;
+        ((*self.weight.get(name).unwrap() as i32) + delta) as u32
+    }
+
+    fn programs(&self) -> Vec<&str> {
+        self.weight.keys().map(|s| *s).collect()
+    }
 }
 
 fn read_file(path: &str) -> Result<String> {
@@ -25,35 +109,23 @@ fn read_file(path: &str) -> Result<String> {
     Ok(buffer)
 }
 
-lazy_static! {
-    static ref RE: regex::Regex = Regex::new(
-        r"^(?P<name>\w+) \((?P<weight>\d+)\)(?: -> (?P<children>.+))?$"
-    ).unwrap();
-}
+// TODO: error handling
+fn parse(input: &str) -> Tower {
+    let mut tower = Tower {
+        weight: HashMap::new(),
+        above: HashMap::new(),
+    };
+    let re = Regex::new(r"\w+").unwrap();
+    for line in input.lines() {
+        let mut iter = re.find_iter(&line);
+        let name = iter.next().unwrap().as_str();
+        let weight = iter.next().unwrap().as_str().parse().unwrap();
+        tower.weight.insert(name, weight);
 
-fn parse(line: &str) -> Program {
-    let caps = RE.captures(line).expect("Encountered error with line");
-    let name: String = caps.name("name")
-        .unwrap()
-        .as_str()
-        .to_string();
-    let weight: u32 = caps.name("weight")
-        .unwrap()
-        .as_str()
-        .parse()
-        .expect("Couldn't convert weight to a number.");
-    let children: Vec<String> = caps.name("children")
-        .map_or_else(Vec::new, |m| {
-            m.as_str()
-                .split(", ")
-                .map(|s| s.to_string())
-                .collect()
-        });
-    Program {
-        name: name,
-        weight: weight,
-        children: children,
+        let above = iter.map(|m| m.as_str()).collect();
+        tower.above.insert(name, above);
     }
+    tower
 }
 
 fn main() {
@@ -64,35 +136,53 @@ fn main() {
     }
     let path = &args[1];
     let input = read_file(path).unwrap();
-    let mut indegrees: HashMap<String, u32> = HashMap::new();
+    let tower = parse(&input);
+    println!("part 1: {}", tower.base());
+    println!("part 2: {:?}", tower.correct_weight(tower.wrongest()));
 
-    for line in input.lines() {
-        let program = parse(line);
-        if !indegrees.contains_key(&program.name) {
-            indegrees.insert(program.name, 0);
-        }
-        for name in program.children {
-            let indegree = indegrees.entry(name).or_insert(0);
-            *indegree += 1;
-        }
-    }
-
-    let root = indegrees.iter()
-        .find(|&(_name, &indegree)| indegree == 0)
-        .map(|(name, _indegree)| name)
-        .unwrap();
-    println!("part 1: {}", root);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn tower() -> Tower<'static> {
+        parse("pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)")
+    }
+
     #[test]
-    fn test_parse() {
-        let program = parse("prog1 (123) -> prog2, prog3, prog4");
-        assert_eq!(program.name, "prog1");
-        assert_eq!(program.weight, 123);
-        assert_eq!(program.children, vec!["prog2", "prog3", "prog4"]);
+    fn test_base() {
+        assert_eq!(tower().base(), "tknk");
+    }
+
+    #[test]
+    fn test_weight() {
+        let tower = tower();
+        assert_eq!(tower.tower_weight("cntj"), 57);
+        assert_eq!(tower.tower_weight("ugml"), 251);
+    }
+
+    #[test]
+    fn test_wrongest() {
+        let tower = tower();
+        assert_eq!(tower.wrongest(), "ugml");
+    }
+
+    #[test]
+    fn test_correct_weight() {
+        let tower = tower();
+        assert_eq!(tower.correct_weight("ugml"), 60);
     }
 }
